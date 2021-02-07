09a66144bc8da97fbb43316a64ea264101931a66
Umair Younus <41501844+umairyounus1997@users.noreply.github.com>
2021-02-07 18:58:42 +0500

    as

---
+++ packages/www/src/pages/app.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Router, Link } from "@reach/router";
+import netlifyIdentity from "netlify-identity-widget";
+
+let Dash = () => {
+   
+   const user = netlifyIdentity.currentUser();
+
+    return <div>{ user && user.user_metadata.full.name}</div>
+ }
+
+export default props => {
+    return (
+    <Router>
+    <Dash path="/app" />
+    </Router>
+    );
+};
+  
--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,16 +1,39 @@
-import React, { useEffect } from 'react';
-import { Container, Heading, Button, Flex } from 'theme-ui';
+import React, { useEffect, useState } from 'react';
+import { Container, Heading, Button, Flex, NavLink } from 'theme-ui';
 import netlifyIdentity from 'netlify-identity-widget';
+import { Link } from 'gatsby';
 
 export default props => {
+
+    const [user, setUser] = useState();
+
     useEffect( ()=>{
         netlifyIdentity.init({});
-    });
+            });
+
+            netlifyIdentity.on("login", user =>{
+                netlifyIdentity.close();
+                setUser(user);
+            });
+
+            netlifyIdentity.on("logout", () => {
+                netlifyIdentity.close();
+                setUser();
+            });
+
     return (
     <Container>
-        <Flex sx={{ flexDirection: "column", padding:3 }}>
-        <Heading as="h1">Todo App</Heading>
-        <Button sx={{ marginTop:2, color: 'black' }}
+             <Flex as='nav'>
+            <NavLink as={Link} to="/" p={2}>
+                Home
+            </NavLink>
+            <NavLink as={Link} to={"/app"} p={2}>
+                Dashboard
+            </NavLink>
+            </Flex>       
+            <Flex sx={{ flexDirection: "column", padding:3 }}>
+            <Heading as="h1">Todo App</Heading>
+            <Button sx={{ marginTop:2, color: 'black' }}
             onClick={ () => { netlifyIdentity.open() } }
         >
             Login
